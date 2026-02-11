627c681f73f3b2f595e5626f5fee268c57846dc7
Steven <erewhon@flatland.org>
2026-02-11 02:05:55 +0000

    Update nous cask to 2026.2.6

--- Casks/nous.rb
+++ Casks/nous.rb
@@ -1,8 +1,8 @@
 cask "nous" do
-  version "2026.2.5"
-  sha256 "8bb2e1dc1541093e3abc20d23a322b870f0f52fc4268b2aa86020bf28139016c"
+  version "2026.2.6"
+  sha256 "4728c27963e0d25951be929eceef06ccfa196ca7d4428652d3696feb7ffe5853"
 
-  url "https://github.com/erewhon/nous/releases/download/v2026.2.5/Nous_2026.2.5_aarch64.dmg"
+  url "https://github.com/erewhon/nous/releases/download/v2026.2.6/Nous_2026.2.6_aarch64.dmg"
   name "Nous"
   desc "Personal notebook and data organization tool"
   homepage "https://github.com/erewhon/nous"
